fc07075c66f644ebca3361abbb517435a76d4d91
Thomas Juhnke <tommy@van-tomas.de>
2020-07-25 17:53:34 +0200

    Introduce Azure Blob Storage SDK recorder sub-module

---
+++ azure-blob-storage-sdk/abss-recorder/src/test/kotlin/de/dreadlabs/HelloTest.kt
@@ -0,0 +1,12 @@
+package de.dreadlabs
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+    @Test
+    fun `it runs`() {
+        assertEquals(true, true)
+    }
+}
